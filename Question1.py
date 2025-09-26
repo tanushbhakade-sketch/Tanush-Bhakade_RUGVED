58b205123b83bbae990b29e550b84755694f4923
Tanush <tanushbhakade@gmail.com>
2025-09-27 00:16:09 +0530

    Question 1 solution completed

---
+++ Question1.py
@@ -0,0 +1,6 @@
+def triple_and(a, b, c):
+    return a and b and c
+
+print(triple_and(True, True, True))   # Output: True
+print(triple_and(True, False, True))  # Output: False
+print(triple_and(False, False, False))# Output: False
