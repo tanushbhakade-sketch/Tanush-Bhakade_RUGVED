399b0a8e2268a1f6233e101b8fa74e8ac6e4eba8
Tanush <tanushbhakade@gmail.com>
2025-09-27 00:33:53 +0530

    Solution to Question 9

---
+++ Question9.py
@@ -0,0 +1,21 @@
+def caesar_cipher(text, shift):
+    result = ""
+
+    for char in text:
+      
+        if char.isupper():
+            result += chr((ord(char) - 65 + shift) % 26 + 65)
+      
+        elif char.islower():
+            result += chr((ord(char) - 97 + shift) % 26 + 97)
+      
+        else:
+            result += char
+
+    return result
+
+
+
+
+        
+        
