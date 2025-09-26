4fd4537821cdf7d83bc6242b90869ea8cf41ac39
Tanush <tanushbhakade@gmail.com>
2025-09-27 00:23:02 +0530

    Solution to question 4

---
+++ Question4.py
@@ -0,0 +1,17 @@
+def selection_sort_string(s):
+ 
+    chars = list(s)
+    
+    
+    n = len(chars)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if chars[j] < chars[min_index]:
+                min_index = j
+
+        chars[i], chars[min_index] = chars[min_index], chars[i]
+
+    return ''.join(chars)
+
+
